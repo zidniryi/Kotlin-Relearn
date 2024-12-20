a42c0d636108e15e2772dcf1e178e51d37f83952
zidniryi <zidnirydwan@gmail.com>
2024-12-20 20:47:16 +0700

    feat: basic compose

--- app/src/main/java/com/example/kotlinrelearn/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrelearn/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,7 +37,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    print("Hello")
 }
+
+@Composable
+fun  Hello(textName: String, modifier: Modifier = Modifier){
+    Button(onClick = { onClick() }) {
+        Text(textName)
+    }
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
