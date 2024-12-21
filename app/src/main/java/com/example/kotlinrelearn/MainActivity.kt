1c51996387bb29a9dfb63a018ea60d45179d6b2e
zidniryi <zidnirydwan@gmail.com>
2024-12-21 13:38:50 +0700

    feat: create ui username

--- app/src/main/java/com/example/kotlinrelearn/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrelearn/MainActivity.kt
@@ -5,21 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -40,7 +25,6 @@ class MainActivity : ComponentActivity() {
                 }
 
             })
-            RegisterScreen(navController)
         }
     }
 }
--- app/src/main/java/com/example/kotlinrelearn/RegisterScreen.kt
+++ app/src/main/java/com/example/kotlinrelearn/RegisterScreen.kt
@@ -1,13 +1,83 @@
 package com.example.kotlinrelearn
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen(navController: NavController){
-    Column {
-        Text("Register  ")
+    var username by remember {
+        mutableStateOf("")
+    }
+    Column (horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth().padding(16.dp), ) {
+        Spacer(modifier = Modifier.height(40.dp))
+        Text("Create username", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Pick username for your account. You can always change it later", fontSize = 16.sp, textAlign = TextAlign.Center)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Password TextField
+        TextField(
+            value = username,
+            onValueChange = {username = it},
+            placeholder = { Text("Username") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .border(
+                    width = 1.dp,
+                    color = Color.Gray,
+                    shape = MaterialTheme.shapes.small
+                )
+                .padding(4.dp), // Optional padding inside the border
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                containerColor = Color(0xFFF9F9F9),
+                unfocusedIndicatorColor = Color.Transparent, // Remove bottom underline
+                focusedIndicatorColor = Color.Transparent // Remove bottom underline
+            )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Login Button
+        Button(
+            onClick = { /* Handle login logic */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF3897F0))
+        ) {
+            Text(text = "Next", color = Color.White, fontSize = 16.sp)
+        }
+
     }
 }
